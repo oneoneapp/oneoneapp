c1ea871de30e1b325c4dad25d0723cc6a3d50765
prince-of-death <developer.acc.2006@gmail.com>
2025-09-01 14:56:11 +0530

    fix: exeption while status code 400

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,9 +3,14 @@ import 'package:talker_dio_logger/talker_dio_logger_interceptor.dart';
 import 'package:talker_dio_logger/talker_dio_logger_settings.dart';
 
 class ApiService {
+  final String baseUrl = 'https://api.oneoneapp.in/';
   final Dio dio = Dio();
 
   void init() {
+    dio.options.headers['Content-Type'] = 'application/json';
+    dio.options.validateStatus = (status) {
+      return status != null && status >= 200 && status < 500;
+    };
     dio.interceptors.add(
       TalkerDioLogger(
         settings: const TalkerDioLoggerSettings(
