a3cf4975859dd4742cfbd7115b03fb10899b3dc9
prince-of-death <developer.acc.2006@gmail.com>
2025-09-01 14:55:31 +0530

    feat: add auth service to locator

--- lib/core/config/locator.dart
+++ lib/core/config/locator.dart
@@ -1,10 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:one_one/services/api_service.dart';
+import 'package:one_one/services/auth_service.dart';
 export 'package:one_one/services/api_service.dart';
+export 'package:one_one/services/auth_service.dart';
 
 final GetIt loc = GetIt.instance;
 
 void setupLocator() {
-  loc.registerSingleton(() => ApiService());
+  loc.registerSingleton<ApiService>(ApiService());
   loc<ApiService>().init();
+  loc.registerSingleton<AuthService>(AuthService(apiService: loc()));
+  loc<AuthService>().init();
 }
