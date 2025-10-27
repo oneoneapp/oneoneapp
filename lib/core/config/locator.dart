7ab151b3918b9fd1c5ebb8847cb4667b810b9cad
prince-of-death <developer.acc.2006@gmail.com>
2025-10-27 20:27:35 +0530

    feat: change intialisations in setup locator

--- lib/core/config/locator.dart
+++ lib/core/config/locator.dart
@@ -1,4 +1,6 @@
 import 'package:get_it/get_it.dart';
+import 'package:one_one/providers/home_provider.dart';
+import 'package:one_one/providers/walkie_talkie_provider.dart';
 import 'package:one_one/services/api_service.dart';
 import 'package:one_one/services/auth_service.dart';
 import 'package:one_one/services/fcm_service.dart';
@@ -7,10 +9,14 @@ export 'package:one_one/services/auth_service.dart';
 
 final GetIt loc = GetIt.instance;
 
-void setupLocator() {
+Future<void> setupLocator() async {
   loc.registerSingleton<ApiService>(ApiService());
   loc<ApiService>().init();
   loc.registerSingleton<FcmService>(FcmService());
+  await loc<FcmService>().initialise();
   loc.registerSingleton<AuthService>(AuthService(apiService: loc(), fcmService: loc()));
   loc<AuthService>().init();
+  loc.registerSingleton(WalkieTalkieProvider());
+  await loc<WalkieTalkieProvider>().initialize();
+  loc.registerSingleton(HomeProvider(walkieTalkieProvider: loc()));
 }
