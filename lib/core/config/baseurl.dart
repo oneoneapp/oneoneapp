f46001acaaafb04d7959d7fe8e77387b9cefd0e2
prince-of-death <developer.acc.2006@gmail.com>
2025-11-22 11:53:43 +0530

    fix: set default url to remote url

--- lib/core/config/baseurl.dart
+++ lib/core/config/baseurl.dart
@@ -1,7 +1,5 @@
 const String baseUrlRemote = "https://api.oneoneapp.in/";
 const String baseUrlLocal = "http://192.168.220.13:5050/";
 
-// Use --dart-define=USE_REMOTE=true to select the remote URL at compile time.
-// Example: flutter run --dart-define=USE_REMOTE=true
-const bool _useRemote = bool.fromEnvironment('USE_REMOTE', defaultValue: false);
+const bool _useRemote = bool.fromEnvironment('USE_REMOTE', defaultValue: true);
 const String baseUrl = _useRemote ? baseUrlRemote : baseUrlLocal;
